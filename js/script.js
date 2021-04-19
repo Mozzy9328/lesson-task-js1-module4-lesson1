bc170b047ccdd344df734bae2f998590cbf97192
Mozzy9328 <74562419+Mozzy9328@users.noreply.github.com>
2021-04-19 20:37:59 +0200

    Update

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const url= "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/nbateams"
+
+const container = document.querySelector(".results")
+async function newFunction(){
+    try{
+    const response = await fetch(url)
+
+    const result = await response.json()
+
+    const data = result.data
+
+    for(i = 0; i <= data.length; i++){
+        const name = data[i].name
+        const city = data[i].city
+        if(data[i].id <= 15 && name.startsWith("C") === false
+        ){
+            container.innerHTML += 
+            `<div class="card">
+            <div> Name: ${name} </div>
+            <div> City: ${city} </div>
+            </div>`
+        }
+    
+}
+    }
+catch(error){
+    console.log(error)
+}
+}
+newFunction();
